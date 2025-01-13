c67d5c0a664ac65e1ffb5236e80371cb93533025
NanaKhodes <ekwerenancy266@gmail.com>
2025-01-13 11:01:27 +0100

    fixed the footer links and the icons

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,29 +1,130 @@
-import React from 'react'
-import style from './Footer.module.css'
+import React from "react";
+import style from "./Footer.module.css";
 import { GiClover } from "react-icons/gi";
+import { TbBrandFacebookFilled } from "react-icons/tb";
+import { FaInstagram } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
       <div className={style.one}>
-          <GiClover size={32} />
+        <GiClover size={50} />
+        <ul>
+          <li>
+            <a href="">SHOP</a>
+          </li>
+          <li>
+            <a href="">BLOG</a>
+          </li>
+          <li>
+            <a href="">ABOUT</a>
+          </li>
+          <li>
+            <a href="">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={style.two}>
+        <div className={style.list}>
+          <h5>Link</h5>
           <ul>
             <li>
-              <a href="">SHOP</a>
+              <a href="#">Link</a>
             </li>
             <li>
-              <a href="">BLOG</a>
+              <a href="#">Link</a>
             </li>
             <li>
-              <a href="">ABOUT</a>
-            </li>
-            <li>
-              <a href="">CONTACT</a>
+              <a href="#">Link</a>
             </li>
           </ul>
+        </div>
+
+        <div className={style.list}>
+          <h5>Link</h5>
+          <ul>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={style.list}>
+          <h5>Link</h5>
+          <ul>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={style.list}>
+          <h5>Link</h5>
+          <ul>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={style.list}>
+          <h5>Link</h5>
+          <ul>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={style.list}>
+          <h5>Link</h5>
+          <ul>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={style.icons}>
+        <TbBrandFacebookFilled size={25} />
+        <FaInstagram size={25} />
+       <SiGmail size={25} />
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
