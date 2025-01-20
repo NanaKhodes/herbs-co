8dafd8ec9a079d63d1d57bb5e20b4724caf5c207
NanaKhodes <ekwerenancy266@gmail.com>
2025-01-20 20:15:50 +0100

    I updated my json fileand also created a pagination functionality to improve user experience

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,38 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import styles  from "./Shop.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "./Shop.module.css";
 
 const Shop = () => {
-  const [shop, setShop] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
 
-   useEffect(() => {
-     const fetchData = async () => {
-       try {
-         const response = await fetch("./herbs.json");
-         const data = await response.json();
-         setShop(data);
-       } catch (error) {
-         console.error("Error fetching data:", error);
-       }
-     };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./herbs.json");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-     fetchData();
-   }, []); 
+    fetchData();
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(products.length / itemsPerPage);
   return (
     <div className={styles.shop}>
       <ul>
-        {shop.map((shop) => (
-          <div key={shop.id} className={styles.item}>
+        {currentProducts.map((product) => (
+          <div key={product.id} className={styles.item}>
             <img src="" alt="" />
-            
+
             <div className={styles.details}>
-              <p>{shop.name}</p>
-              <p>{shop.price}</p>
+              <p>{product.name}</p>
+              <p>{product.price}</p>
             </div>
           </div>
         ))}
       </ul>
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((number) => (
+          <button
+            key={number}
+            onClick={() => handlePageChange(number)}
+            className={currentPage === number ? "active" : ""}
+          >
+            {number}
+          </button>
+        ))}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default Shop
+export default Shop;
