9abbdc21b9a9f07e08cd88e23d7cd09e6eea8c12
NanaKhodes <ekwerenancy266@gmail.com>
2025-01-20 18:53:22 +0100

    fetched and displayyed the json file in the shop component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles  from "./Shop.module.css"
 
 const Shop = () => {
-  
-  return (
-    <div>
+  const [shop, setShop] = useState([]);
 
+   useEffect(() => {
+     const fetchData = async () => {
+       try {
+         const response = await fetch("./herbs.json");
+         const data = await response.json();
+         setShop(data);
+       } catch (error) {
+         console.error("Error fetching data:", error);
+       }
+     };
+
+     fetchData();
+   }, []); 
+  return (
+    <div className={styles.shop}>
+      <ul>
+        {shop.map((shop) => (
+          <div key={shop.id} className={styles.item}>
+            <img src="" alt="" />
+            
+            <div className={styles.details}>
+              <p>{shop.name}</p>
+              <p>{shop.price}</p>
+            </div>
+          </div>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Shop
